55be2a4833738456de2f3799cf2bef4cddf8c25c
Albert <albert@montech.io>
2024-09-29 13:09:20 +0100

    chore: update query config

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,9 +1,36 @@
 "use client";
+
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
 
 export function ReactQueryProvider({ children }: PropsWithChildren) {
-  const queryClient = new QueryClient();
+  const queryClient = getQueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
