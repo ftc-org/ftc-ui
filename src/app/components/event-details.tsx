b439b6f28dcbe5a509a2bdaec7d62f5c926600f4
Albert <albert.dugba@gmail.com>
2024-10-01 12:14:35 +0100

    fix: static paths for event

---
+++ src/app/components/event-details.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { useGetEvent } from "@/api/get-events";
+import Loader from "@/app/components/loader";
+import { getFormattedDate } from "@/utils/date";
+
+type Props = {
+  eventId: string;
+};
+export function EventDetailsPage({ eventId }: Props) {
+  const res = useGetEvent({ id: eventId as string });
+
+  if (res.isLoading) {
+    return (
+      <div className='max-w-screen-xl mx-auto px-3 min-h-[90vh] flex justify-center items-center'>
+        <Loader />
+      </div>
+    );
+  }
+
+  return (
+    <div className='max-w-screen-xl mx-auto px-3'>
+      <div>
+        <h1 className='text-lg font-bold tracking-tight lg:text-3xl text-aljazeera-red'>
+          {res.data?.title}
+        </h1>
+        <p>{res.data?.description}</p>
+      </div>
+      <hr className='my-5' />
+      <div>
+        {res.data && res.data.updates.length <= 0 ? (
+          <div className='flex justify-center items-center'>
+            <p className='text-neutral-500'>No Updates to display</p>
+          </div>
+        ) : (
+          <div>
+            <h1 className='text-lg font-bold tracking-tight lg:text-xl text-aljazeera-red'>
+              Updates
+            </h1>
+            <br />
+            <div>
+              <div className='bg-white p-4 w-full rounded-b-xl'>
+                <div className='mt-4 relative'>
+                  <div className='absolute left-[5px] top-[9px] bottom-[9px] w-px bg-orange-300'></div>
+                  {res.data?.updates.map((item, index) => (
+                    <div key={index} className='flex items-start mb-5 relative'>
+                      <div className='absolute left-0 mt-2'>
+                        <div className='size-[10px] bg-orange-500 rounded-full ring-4 ring-orange-200'></div>
+                      </div>
+                      <div className='ml-8'>
+                        <span className='text-sm text-gray-600 font-medium'>
+                          {getFormattedDate(item.created_at)}
+                        </span>
+                        <p>{item.content}</p>
+                      </div>
+                    </div>
+                  ))}
+                  <div className='absolute left-0 bottom-0'>
+                    <div className='size-[10px] bg-orange-500 rounded-full ring-4 ring-orange-200'></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
