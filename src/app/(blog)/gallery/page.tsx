07544639fa87f7ee7a8b4e97733fdf023a4b922a
Nana K. <73991180+topboyasante@users.noreply.github.com>
2024-10-02 09:23:15 +0000

    gallery-fix

--- src/app/(blog)/gallery/page.tsx
+++ src/app/(blog)/gallery/page.tsx
@@ -2,18 +2,18 @@
 /* eslint-disable @next/next/no-img-element */
 
 import React from "react";
-import { mediaItems } from "@/mocks/gallery";
+import { mediaItems, PATRIOTS } from "@/mocks/gallery";
 import { ImageMasonryLayout } from "@/app/components/masonry-layout";
 
 const GalleryPage = () => {
   return (
-    <div className='min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8'>
+    <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
       <ImageMasonryLayout
-        mediaItems={mediaItems}
+        mediaItems={[...mediaItems, ...PATRIOTS]}
         showVideos={false}
-        videoFrameClassName='lg:h-60 h-50'
-        title='Galamsey in Ghana: A Visual Journey'
-        subtitle='Exploring the impact of illegal small-scale gold mining'
+        videoFrameClassName="lg:h-60 h-50"
+        title="Galamsey in Ghana: A Visual Journey"
+        subtitle="Exploring the impact of illegal small-scale gold mining"
       />
     </div>
   );
