0435f6407cd132790248cd89c54067d69d408d67
Nana K. <73991180+topboyasante@users.noreply.github.com>
2024-10-02 09:02:17 +0000

    ui fix for tweet links

--- src/app/(blog)/components/index.tsx
+++ src/app/(blog)/components/index.tsx
@@ -6,11 +6,11 @@ import { PostCard } from "./post-card";
 
 import { useGetPosts } from "@/api";
 import { useGetEvents } from "@/api/get-events";
+import { LatestContent } from "./latest-content";
+import Link from "next/link";
+import { ChevronRight } from "lucide-react";
 import { ImageMasonryLayout } from "@/app/components/masonry-layout";
 import { PATRIOTS } from "@/mocks/gallery";
-import { ChevronRight } from "lucide-react";
-import Link from "next/link";
-import { LatestContent } from "./latest-content";
 
 function Landing() {
   const { events } = useGetEvents({ isLive: false });
@@ -28,10 +28,17 @@ function Landing() {
     },
   ];
 
+  const handleTweet = (hashtag: string) => {
+    const twitterUrl = `https://x.com/search?q=${encodeURIComponent(
+     `#${hashtag}`
+    )}&src=typed_query&f=top`;
+    window.open(twitterUrl, "_blank", "noopener,noreferrer");
+  };
+
   return (
     <Fragment>
       <div>
-        <h1 className="text-lg font-bold tracking-tight lg:text-3xl text-aljazeera-red">
+        <h1 className='text-lg font-bold tracking-tight lg:text-3xl text-aljazeera-red'>
           #FreeTheCitizens
         </h1>
         <p>
@@ -39,40 +46,37 @@ function Landing() {
           #FreetheCitizens.
         </p>
       </div>
-      <div className="mt-5 flex items-center gap-3 w-full overflow-x-scroll">
+      <div className='mt-5 flex items-center gap-3 w-full overflow-x-scroll'>
         {trendingLinks.map((link, index) => {
           return (
-            <Link
-              href={`https://x.com/search?q=${encodeURIComponent(
-                link.tag
-              )}&src=typed_query&f=top`}
+            <button
               key={index}
-              target="_blank"
-              className="rounded-md px-2 py-1 bg-aljazeera-red text-white hover:text-aljazeera-red hover:bg-white ease duration-200 w-fit grid place-items-center"
+              onClick={() => handleTweet(link.tag)}
+              className='rounded-md px-2 py-1 bg-aljazeera-red text-white hover:text-aljazeera-red hover:bg-white ease duration-200 w-fit grid place-items-center'
             >
-              # {link.tag}
-            </Link>
+             #{link.tag}
+            </button>
           );
         })}
       </div>
-      <div className="flex lg:flex-row flex-col gap-10">
-        <div className="mt-10 lg:w-6/12 w-full">
+      <div className='flex lg:flex-row flex-col gap-10'>
+        <div className='mt-10 lg:w-6/12 w-full'>
           <LiveUpdateCard />
         </div>
 
-        <div className="lg:w-5/12">
-          <ul className="mt-10 flex-1 grid md:grid-cols-2 grid-cols-1 h-fit gap-7">
+        <div className='lg:w-5/12'>
+          <ul className='mt-10 flex-1 grid md:grid-cols-2 grid-cols-1 h-fit gap-7'>
             {events?.map((event, index) => (
               <PostCard item={event} key={index} />
             ))}
           </ul>
 
-          <ul className="mt-4 md:block hidden">
-            <div className="flex items-center justify-between">
-              <h1 className="my-2 text-lg">Media</h1>
+          <ul className='mt-4 md:block hidden'>
+            <div className='flex items-center justify-between'>
+              <h1 className='my-2 text-lg'>Media</h1>
               <Link
-                href="/gallery"
-                className="my-2 text-lg flex items-center gap-1"
+                href='/gallery'
+                className='my-2 text-lg flex items-center gap-1'
               >
                 <span> See All </span>
                 <ChevronRight />
@@ -85,12 +89,12 @@ function Landing() {
 
       {posts && posts?.length === 0 ? null : (
         <div>
-          <LatestContent type="Posts" />
+          <LatestContent type='Posts' />
         </div>
       )}
 
-      <div className="py-10">
-        <LatestContent type="Events" />
+      <div className='py-10'>
+        <LatestContent type='Events' />
       </div>
     </Fragment>
   );
